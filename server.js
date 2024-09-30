e198cef0ae9475e7bcb7389f86a796d003df0e2a
Pauline Delacour <pauline.delacour33@gmail.com>
2024-09-30 16:37:11 +0200

    Creation du fichier server

---
+++ server.js
@@ -0,0 +1,53 @@
+// creer un programme qui va attendre des requetes HTPP , importer le package HTTP de node 
+const http = require('http');
+//importation de l'application express 
+const app = require('./app');
+
+//Fonction qui renvoie un port valide, qu'il foit fourni sous la forme d'un numéro ou d'une chaine
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3100');
+app.set('port', port);
+
+//Fonction qui recherche les différentes erreurs et les gère de manière appropriée, qui est ensuite enregistrée dans le serveur
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
+
+
