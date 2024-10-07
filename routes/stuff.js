866a50f752b8c6cfad702efa3ba9113d6b40c967
Pauline Delacour <pauline.delacour33@gmail.com>
2024-10-07 15:58:58 +0200

    ajout du middleware d'authentification

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,20 +1,22 @@
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
+
 const stuffControllers = require("../controllers/stuff");
 
+// Requete GET qui recupére la liste de tout les objets
+router.get("/", auth, stuffControllers.getAllThing);
+
 // Requete POST pour creer un nouvel objet
-router.post("/", stuffControllers.createThing);
-
-// Requete PUT qui permet de modifier un objet par son id
-router.put("/:id", stuffControllers.modifyThing);
-
-// Requete DELETE pour supprimer un obet par son id 
-router.delete("/:id", stuffControllers.deleteThing);
+router.post("/", auth, stuffControllers.createThing);
 
 // Requete GET pour récupérer un objet par son id
-router.get("/:id", stuffControllers.getOneThing);
+router.get("/:id", auth, stuffControllers.getOneThing);
 
-// Requete GET qui recupére la liste de tout les objets
-router.get("/", stuffControllers.getAllThing);
+// Requete PUT qui permet de modifier un objet par son id
+router.put("/:id", auth, stuffControllers.modifyThing);
+
+// Requete DELETE pour supprimer un obet par son id
+router.delete("/:id", auth, stuffControllers.deleteThing);
 
 module.exports = router;
