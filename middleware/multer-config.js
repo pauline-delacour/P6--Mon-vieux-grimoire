d735ece397d68157453c1afd5c7ea047316b37bc
Pauline Delacour <pauline.delacour33@gmail.com>
2024-10-09 17:03:00 +0200

    Utilisation de memory storage a la place de diskstorage

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -8,22 +8,8 @@ const MIME_TYPES = {
   "image/png": "png",
 };
 
+
 // Configuration du chemin et le nom de fichier pour les fichiers entrants
-const storage = multer.diskStorage({
-    //Le fichier destination la ou seront envoyé les fichiers telechargées 
-  destination: (req, file, callback) => {
-    // les fichiers seront enregistré dans le dossier image
-    callback(null, "images");
-  },
-  
-  filename: (req, file, callback) => {
-    //Remplace les espaces dans le nom original du fichier par des underscores
-    const name = file.originalname.split(" ").join("_");
-    //Recupére l'extension correpondante a partir du type MIME du fichier 
-    const extension = MIME_TYPES(file.mimetype);
-    // Crée un noù de fichier unique en ajoutant un timestamp (date.now()) pour garantir l'unicité du nom + ajout de l'extension appropriée
-    callback(null, name + Date.now() + "." + extension);
-  },
-});
+const storage = multer.memoryStorage();
 
 module.exports = multer({ storage }).single("image");
