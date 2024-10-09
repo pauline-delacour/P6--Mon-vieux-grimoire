00e7dca8e284c3e2984044adc28df624c701c3ac
Pauline Delacour <pauline.delacour33@gmail.com>
2024-10-09 17:02:28 +0200

    Utilisation de dotenv = ajout de commentaire

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,19 @@
 const jwt = require ('jsonwebtoken');
+require ('dotenv').config();
 
+
+// Middleware d'authentification 
 module.exports = (req, res, next) => {
     try {
+        //Récuperation du token JWT depuis l'en-tete authorization 
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        //Vérification et decodage du token à l'aide de la clé secrète 
+        const decodedToken = jwt.verify(token,process.env.TOKEN);
+        //Récupération de l'identifiant utilisateur (userId) du token décodé
         const userId = decodedToken.userId;
+        //Ajout de l'id au requete pour qu'il soit accessible pour les prochaines etapes
         req.auth = {
-            userId: userId
+            userId: userId // Stocke l'id dans l'objet auth de la requete
         };
         next();
     } catch (error) {
