aa967a5ea908a1712e7619f8c74e06990ba97484
Pauline Delacour <pauline.delacour33@gmail.com>
2024-10-11 18:01:24 +0200

    Ajout de commentaire

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userControllers = require('../controllers/user');
 
+// Requete POST signup
 router.post('/signup', userControllers.signup);
+
+// Requete POST login
 router.post('/login', userControllers.login);
 
 module.exports = router; 
